c0cf932bcc77d2f78841a30ddbd4e1b9033f4b39
Maksim Makarenko <maksmak91@MacBook-Pro.local>
2025-11-29 14:21:21 +0300

    done money like cents number

---
+++ pkg/types/moneys/money.go
@@ -0,0 +1,77 @@
+package moneys
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Money struct {
+	amount int64 // cents number
+}
+
+func (m Money) String() string {
+	return fmt.Sprintln(m.amount)
+}
+
+func (m Money) MarshalJSON() ([]byte, error) {
+	units, cents := m.amount/100, m.amount%100
+	if cents < 0 {
+		cents *= -1
+	}
+
+	res := fmt.Sprintf("%d.%02d", units, cents)
+	if cents < 10 {
+		res = fmt.Sprintf("%d.0%d", units, cents)
+	}
+
+	res = strings.TrimRight(res, "0")
+	res = strings.TrimRight(res, ".")
+	return []byte(res), nil
+}
+
+func (m *Money) UnmarshalJSON(data []byte) error {
+	if bytes.HasPrefix(data, []byte{'-'}) {
+		return fmt.Errorf("negative value")
+	}
+	value := string(data)
+
+	dotIndex := strings.Index(value, ".")
+	if dotIndex == -1 {
+		// There's not dot
+		units, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return err
+		}
+		m.amount = units * 100
+		return nil
+	}
+
+	unitsStr := value[:dotIndex]
+	centsStr := value[dotIndex+1:]
+
+	if len(centsStr) == 1 {
+		centsStr += "0"
+	}
+
+	units, err := strconv.ParseInt(unitsStr, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	cents, err := strconv.ParseInt(centsStr, 10, 64)
+	if err != nil {
+		return err
+	}
+	if cents < 0 || cents >= 100 {
+		return fmt.Errorf("invalid cents")
+	}
+	if units < 0 {
+		cents *= -1
+	}
+
+	m.amount = units*100 + cents
+	return nil
+
+}
