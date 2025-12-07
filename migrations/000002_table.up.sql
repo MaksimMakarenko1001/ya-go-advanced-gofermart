7d476d2f55ec757b7a34048a2cefc0232a91640e
Maksim Makarenko <maksmak91@172.31.110.246-vpn.dhcp.yndx.net>
2025-12-07 20:05:51 +0300

    addes foreign key into tables

--- migrations/000002_table.up.sql
+++ migrations/000002_table.up.sql
@@ -1,34 +1,34 @@
-CREATE TABLE IF NOT EXISTS order.orders (
+CREATE TABLE IF NOT EXISTS orders.orders (
     id SERIAL PRIMARY KEY,
     order_number TEXT UNIQUE NOT NULL,
     order_status TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS order.accruals (
+CREATE TABLE IF NOT EXISTS orders.accruals (
     id SERIAL PRIMARY KEY,
     accrual_status TEXT NOT NULL,
     accrual_amount BIGINT NOT NULL DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
-    order_id INTEGER UNIQUE NOT NULL
+    order_id INTEGER UNIQUE NOT NULL,
 
-    FOREIGN KEY (order_id) REFERENCES order.orders(id)
+    FOREIGN KEY (order_id) REFERENCES orders.orders(id)
 );
 
-CREATE TABLE IF NOT EXISTS order.withdrawals (
+CREATE TABLE IF NOT EXISTS orders.withdrawals (
     id SERIAL PRIMARY KEY,
     withdrawal_amount BIGINT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
-    order_id INTEGER UNIQUE NOT NULL
+    order_id INTEGER UNIQUE NOT NULL,
 
-    FOREIGN KEY (order_id) REFERENCES order.orders(id)
+    FOREIGN KEY (order_id) REFERENCES orders.orders(id)
 );
 
-CREATE TABLE IF NOT EXISTS order.user_balances (
+CREATE TABLE IF NOT EXISTS orders.user_balances (
     id SERIAL PRIMARY KEY,
     accrual_amount BIGINT NOT NULL,
     withdrawal_amount BIGINT NOT NULL,
